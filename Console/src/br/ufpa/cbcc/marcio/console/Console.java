f419d61ae8f5995edd4ea6e7fa3e74f68345f834
Marcio Reis <mahrcio@msn.com>
2016-04-13 16:10:51 -0300

    Update Console.java

--- Console/src/br/ufpa/cbcc/marcio/console/Console.java
+++ Console/src/br/ufpa/cbcc/marcio/console/Console.java
@@ -1,6 +1,16 @@
 package br.ufpa.cbcc.marcio.console;
 
 public class Console {
+String marca;
+
+	public Console(){
+	marca="Desconhecida";
+	
+	}
+	
+	public Console(String marca){
+	this.marca=marca;
+	}
 	
 	public static void main(String[] args){
 		System.out.println("Alo Mundo");
