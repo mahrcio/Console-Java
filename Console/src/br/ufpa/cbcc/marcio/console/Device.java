02d3f41965d123722a5cc11da629e3bef945559f
Marcio Reis <mahrcio@msn.com>
2016-04-13 15:37:42 -0300

    Update Device.java

--- Console/src/br/ufpa/cbcc/marcio/console/Device.java
+++ Console/src/br/ufpa/cbcc/marcio/console/Device.java
@@ -1,5 +1,10 @@
 package br.ufpa.cbcc.marcio.console;
 
 public abstract class Device {
+  private String tipoDevice;
+    public Device(String tipoDevice)
+      {
+        this.tipoDevice=tipoDevice;
+      }
 
 }
