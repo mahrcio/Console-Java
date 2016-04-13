21e7d60888ea4eb41670b6a41ebec558cf5084d6
Marcio Reis <mahrcio@msn.com>
2016-04-13 16:06:24 -0300

    Update Psp.java

--- Console/src/br/ufpa/cbcc/marcio/console/Psp.java
+++ Console/src/br/ufpa/cbcc/marcio/console/Psp.java
@@ -1,5 +1,12 @@
 package br.ufpa.cbcc.marcio.console;
 
 public class Psp {
+  bool wifiOn = false;
+  @Override
+	public boolean logar(String usuario, String senha) {
+		if ((usuario=="Marcio") && (senha=="123456") && wifiOn)
+			return true;
+		return false;
+	}
 
 }
